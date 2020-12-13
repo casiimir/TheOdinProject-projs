5ff4832a74c22d2d4fbadc3463c280b5287f220e
casiimir <73138891+casiimir@users.noreply.github.com>
2020-12-13 17:54:42 +0100

    Upload version beta 1

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,60 +1,46 @@
-function drawGrid (dim) {
-  for (let x = 1; x <= (256*256) / (dim*dim); x++) {
+import { pencil, rubber, clear, showGrid, fill, setColor } from "./tools.js";
+
+function setCanvas (squaresDim) {
+  for (let x = 1; x <= (256*256) / (squaresDim*squaresDim); x++) {
     const div = document.createElement('div');
     div.classList.add('square');
     mainContainer.appendChild(div);
   }
 }
 
-function setGridSquares (dim) {
+function setGridSquares (squaresDim) {
   const divs = document.querySelectorAll('.square');
   divs.forEach(div => {
-    div.style.width = `${dim}px`;
-    div.style.height = `${dim}px`;
+    div.style.width = `${squaresDim}px`;
+    div.style.height = `${squaresDim}px`;
   })
 }
 
-function hoverOnSquares() {
-  const divs = document.querySelectorAll('.square');
-  divs.forEach(div => {
-    div.addEventListener('click', (e) => {
-      div.style.backgroundColor = color;
-    });
-  })
-}
-
-function clearTheGrid() {
-  clearButton.addEventListener('click', () => {    
-    const divs = document.querySelectorAll('.square');
-    divs.forEach(div => {
-      div.style.backgroundColor = 'transparent';
-    })
-    gridDimension = prompt('How many squares per side?\nPut: 8, 16, 32, 64 or 128');
-    if (gridDimension <= 128 && gridDimension >= 8) {
-      drawNewGrid();
-    } else {
-      gridDimension = prompt('Min size: 8px, Max size 128px\nPut: 8, 16, 32, 64 or 128');
-    }
-  })
-}
-
-function drawNewGrid () {
-  drawGrid(gridDimension);
-  setGridSquares(gridDimension);
-  hoverOnSquares();
-}
-
-function setColor () {
-  pickColor.addEventListener('change', (e) => color = e.target.value);
+function init () {
+  setCanvas(squaresDim);
+  setGridSquares(squaresDim);
 }
 
 // Get all elements from the DOM
 const mainContainer = document.querySelector('.main-container');
-const clearButton = document.querySelector('.btn');
-const pickColor = document.querySelector('.color');
-let color = 'violet';
-let gridDimension = 32;
+let squaresDim = 32;
 
-drawNewGrid();
-clearTheGrid();
-setColor();
+const buttonClear = document.querySelector('.btn-clear');
+buttonClear.addEventListener('click', () => clear());
+
+const buttonPencil = document.querySelector('.btn-pencil');
+buttonPencil.addEventListener('click', () => pencil());
+
+const buttonRubber = document.querySelector('.btn-rubber');
+buttonRubber.addEventListener('click', () => rubber());
+
+const buttonGrid = document.querySelector('.btn-grid');
+buttonGrid.addEventListener('click', () => showGrid());
+
+const buttonFill = document.querySelector('.btn-fill');
+buttonFill.addEventListener('click', () => fill());
+
+const buttonColor = document.querySelector('.color');
+buttonColor.addEventListener('change', () => setColor());
+
+init();
---
+++ etch-a-sketch/tools.js
@@ -0,0 +1,55 @@
+function pencil () {
+  const pickColor = document.querySelector('.color');
+  let color = pickColor.value;
+  pickColor.addEventListener('change', (e) => color = e.target.value);
+
+  const divs = document.querySelectorAll('.square');
+  divs.forEach(div => {
+    div.addEventListener('click', (e) => {
+      div.style.backgroundColor = color;
+    });
+  })
+}
+
+function rubber () {
+  const divs = document.querySelectorAll('.square');
+  divs.forEach(div => {
+    div.addEventListener('click', () => {
+      div.style.backgroundColor = 'transparent';
+    });
+  })
+}
+
+function clear() {
+  const divs = document.querySelectorAll('.square');
+  divs.forEach(div => {
+    div.style.backgroundColor = 'transparent';
+  })
+}
+
+function showGrid() {  
+  const divs = document.querySelectorAll('.square'); 
+  divs.forEach(div => {
+    div.classList.toggle('square-grid');
+  })
+}
+
+function fill () {  
+  const pickColor = document.querySelector('.color');
+  let color = pickColor.value;
+  pickColor.addEventListener('change', (e) => color = e.target.value);
+
+  const divs = document.querySelectorAll('.square'); 
+  divs.forEach(div => {
+    div.style.backgroundColor = color;
+  })
+}
+
+function setColor () {  
+  const pickColor = document.querySelector('.color'); 
+  pickColor.addEventListener('change', (e) => color = e.target.value); 
+  let color = pickColor.value;
+  pickColor.style.backgroundColor = color;
+}
+
+export { pencil, rubber, clear, showGrid, fill, setColor };
